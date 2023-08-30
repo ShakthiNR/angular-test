f9502b1298038663fa879564225570f1ca4facc3
shakthinr <shakthinaarayanan@gmail.com>
2023-08-30 20:26:55 +0530

    Half Baked

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms"
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent {
+
+  constructor() {}
+
+  handleSubmit(formData:NgForm){
+    console.log(formData)
+  }
+
+  handleClear(formData:NgForm){
+    formData.reset()
+  }
+}
